c7806451fc1debd319f767db55374b2074b0b65d
Ayrton Vergara Mondaca <ayrtonvergara50@gmail.com>
2021-05-13 22:48:45 -0600

    generamos la tabla de pokemones dinamicamente

--- js/index.js
+++ js/index.js
@@ -14,8 +14,43 @@ tinymce.init({
     'removeformat | help',
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
   });
-  const pokemones = [];//desfinir un arreglo en javascrips
   
+  const pokemones = [];//desfinir un arreglo en javascrips
+
+  const cargarTabla = ()=>{
+    // 1-una referencia a la tabla( tabla a caragr)
+    let tbody = document.querySelector("#tbody-pokemon");//nota para la clase se pone .tbody-pokemon ve a buscar el documento que tiene el tbody oikemon y guaradalo en let body
+    tbody.innerHTML = ""; // ES PARA LIMPIAR LA EJECUCION DEL CODIGO
+    // 2-por cada pokemon generar una fila
+    for(let i=0; i < pokemones.length; ++ i){
+       let p = pokemones[i];
+       // crea un elemento que no existe, pero no lo agrega a la pagina
+       // puedo crear cualquier etiqueta html aqui
+       let tr = document.createElement("tr");
+       // 3-por cada atributos de los pokemon(nombre ,tipo etc) generar una celda
+       let tdNombre = document.createElement("td");
+       let tdTipo = document.createElement("td");
+       let tdDescripcion = document.createElement("td");
+       let tdNro = document.createElement("td");
+       let tdAcciones = document.createElement("td");
+       //es para poner el texto que va dentro de la celda, osea p
+       tdNombre.innerText = p.nombre;
+       tdTipo.innerText = p.tipo;
+       //todo: esto no va a funcionar a la primera
+       tdDescripcion.innerHTML = p.descripcion;
+       tdNro.innerText = i + 1;
+       // todo:  agregar botton acciones
+       tr.appendChild(tdNro);
+       tr.appendChild(tdNombre);
+       tr.appendChild(tdTipo);
+       tr.appendChild(tdDescripcion);
+       tr.appendChild(tdAcciones);
+       tbody.appendChild(tr);
+       
+    }
+    
+    // 4-agregar esa fila a la tabla(manipulando el DOM)
+  };
  //referencia documento web-buscar dentro de la pag-#pokemon-form-se le agrega un listener-submit es el evento
  //-funciones de llaves( lo que va al medio es el evento)-e.preventdefault(es para que no se recargue la pag.)
  document.querySelector("#pokemon-form").addEventListener('submit', (e)=>{
@@ -30,6 +65,8 @@ tinymce.init({
  pokemon.descripcion = descripcion;
  pokemon.legendario = legendario;
  pokemon.tipo = tipo;
- pokemones.push(pokemon);// objeto dentro de un arreglo
- console.log(pokemones);// muestro la lista
+
+ pokemones.push(pokemon);// datos  dentro de un arreglo
+ cargarTabla();
+ 
  });
